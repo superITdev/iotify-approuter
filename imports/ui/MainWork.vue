8d9452e400a0167d1d3127920ab06c630df59dcf
Vladimir Pavljuchenkov <upwork@spiderx.dp.ua>
2020-10-19 19:24:39 +0700

    data model ...

--- imports/ui/MainWork.vue
+++ imports/ui/MainWork.vue
@@ -16,13 +16,13 @@
       <v-list flat class="pa-0">
         <v-list-item-group mandatory v-model="majorVS.activeMajorType">
           <v-list-item
-            v-for="(major, i) in majorCategories" :key="i"
+            v-for="(major, majorIdx) in majorCategories" :key="majorIdx"
             class="pa-0"
             :color="major.color"
             active-class="white"
             :style="`height:${majorVS.itemHeight}px`"
             :value="major.majorType"
-            @click="onMajorCliick(major)"
+            @click="onMajorCliick(major.majorType)"
           >
             <template v-slot:default="{active}">
               <v-list-item-content>
@@ -57,17 +57,40 @@
             <v-text-field dense outlined prepend-inner-icon="mdi-magnify" clearable/>
           </v-list-item>
 
-          <v-expansion-panels accordion multiple>
+          <v-expansion-panels accordion multiple tile>
             <v-expansion-panel>
               <v-expansion-panel-header class="iotar-subbar-subtitle">Recently Used</v-expansion-panel-header>
               <v-expansion-panel-content class="iotar-subbar-content">
                 node1, node2, node3 ...
               </v-expansion-panel-content>
             </v-expansion-panel>
-            <v-expansion-panel>
-              <v-expansion-panel-header class="iotar-subbar-subtitle">Collection</v-expansion-panel-header>
+            <v-expansion-panel v-for="(sub, subIdx) in activeSubCategories" :key="subIdx">
+              <v-expansion-panel-header class="iotar-subbar-subtitle">{{sub.subTitle}}</v-expansion-panel-header>
               <v-expansion-panel-content class="iotar-subbar-content">
-                node1, node2, node3 ...
+                <v-row>
+                  <v-col>
+                    Item 1
+                  </v-col>
+                  <v-col>
+                    Item 1
+                  </v-col>
+                  <v-col>
+                    Item 1
+                  </v-col>
+                  <v-col>
+                    Item 1
+                  </v-col>
+                  <v-col>
+                    Item 1
+                  </v-col>
+                  <v-col>
+                    Item 1
+                  </v-col>
+                  <v-col>
+                    Item 1
+                  </v-col>
+
+                </v-row>
               </v-expansion-panel-content>
             </v-expansion-panel>
           </v-expansion-panels>
@@ -91,25 +114,229 @@
           itemIconSize: 50, // px
           itemTextSize: 13, // px
 
-          activeMajorType: ""
+          activeMajorType: ''
         },
-        majorCategories: [ // data for major category
+      }
+    },
+    computed: {
+      majorCategories() {
+        const majors = [ // data for major category
           { majorType:'', title: '', icon: 'IoT', color: 'indigo darken-4', globalRecent: true },
           { majorType:'deployment', title: 'Deployment', icon: 'deployment', color: 'blue accent-4' },
           { majorType:'protocol', title: 'Protocol', icon: 'protocol', color: 'green accent-4' },
-          { majorType:'memory', title: 'Memory', icon: 'memory', color: 'orange accent-4' },
-          { majorType:'control', title: 'Control', icon: 'control', color: 'cyan accent-4' },
-          { majorType:'illustration', title: 'Illustration', icon: 'illustration', color: 'purple accent-4' },
-        ],
-        items: [
-          { title: 'Home', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-forum' },
-        ],
+          { majorType:'database', title: 'Database', icon: 'memory', color: 'orange accent-4' },
+          { majorType:'function', title: 'Function', icon: 'control', color: 'cyan accent-4' },
+          { majorType:'staticAsset', title: 'Static Asset', icon: 'illustration', color: 'purple accent-4' },
+        ];
+        return majors;
+      },
+      allNodes() {
+        const nodes = [ // fetched from backend
+          // deployment
+          {
+              majorType: 'deployment',
+              itemTitle: "AWS",
+          },
+          {
+              majorType: 'deployment',
+              itemTitle: "GCP",
+          },
+          {
+              majorType: 'deployment',
+              itemTitle: "Edge",
+          },
+          // protocol
+          // protocol/gateway
+          {
+              majorType: 'protocol',
+              subTitle: 'Gateway',
+              itemTitle: "HTTP",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Gateway',
+              itemTitle: "Websocket",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Gateway',
+              itemTitle: "MQTT",
+          },
+          // protocol/client
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "HTTP",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "Websocket",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "MQTT",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "NATS",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "TCP",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "UDP",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "COAP",
+          },
+          {
+              majorType: 'protocol',
+              subTitle: 'Client',
+              itemTitle: "SSH",
+          },
+          // database
+          // database/client
+          {
+              majorType: 'database',
+              subTitle: 'Client',
+              itemTitle: "REDIS",
+          },
+          {
+              majorType: 'database',
+              subTitle: 'Client',
+              itemTitle: "MongoDB",
+          },
+          {
+              majorType: 'database',
+              subTitle: 'Client',
+              itemTitle: "Elastic search",
+          },
+          {
+              majorType: 'database',
+              subTitle: 'Client',
+              itemTitle: "S3",
+          },
+          // database/server
+          {
+              majorType: 'database',
+              subTitle: 'Server',
+              itemTitle: "REDIS",
+          },
+          // function
+          // function/custom
+          {
+              majorType: 'function',
+              subTitle: 'Custom',
+              itemTitle: "Javascript",
+          },
+          {
+              majorType: 'function',
+              subTitle: 'Custom',
+              itemTitle: "Go",
+          },
+          // function/prebuilt
+          {
+              majorType: 'function',
+              subTitle: 'Prebuilt',
+              itemTitle: "Timer",
+          },
+          {
+              majorType: 'function',
+              subTitle: 'Prebuilt',
+              itemTitle: "IETTT",
+          },
+          {
+              majorType: 'function',
+              subTitle: 'Prebuilt',
+              itemTitle: "Zapier",
+          },
+          {
+              majorType: 'function',
+              subTitle: 'Prebuilt',
+              itemTitle: "Webhook",
+          },
+          {
+              majorType: 'function',
+              subTitle: 'Prebuilt',
+              itemTitle: "REST2Websocket",
+          },
+          {
+              majorType: 'function',
+              subTitle: 'Prebuilt',
+              itemTitle: "MQTT2HTTP",
+          },
+          // staticAsset
+          {
+              majorType: 'staticAsset',
+              itemTitle: "Web URL",
+          },
+          {
+              majorType: 'staticAsset',
+              itemTitle: "Icon",
+          },
+          {
+              majorType: 'staticAsset',
+              itemTitle: "Github",
+          },
+        ]
+        return nodes;
+      },
+      activeSubCategories() {
+        const subs = this.getSubCategories(this.majorVS.activeMajorType);
+        return subs;
       }
     },
     methods: {
-      onMajorCliick(major) {
+      getMajorCategory(majorType) {
+        return this.majorCategories.find(mc => mc.majorType===majorType);
+      },
+      getSubCategories(majorType) {
+        // Filter nodes by majorType.
+        const nodes = this.allNodes.filter(node => node.majorType===majorType);
+
+        const subs = []; // [ {majorType, subTitle, items[ {} ]} ]
+        nodes.forEach(node => {
+          let sub = subs.find((sub) => sub.subTitle==node.subTitle);
+          if (sub) {
+            // Register the node into sub category.
+            sub.items.push(node);
+          } else {
+            // Create new sub cateogory.
+            sub = {
+              majorType: node.majorType,
+              subTitle: node.subTitle,
+              items: [],
+            };
+            sub.items.push(node);
+            subs.push(sub);
+          }
+        });
+        
+        // default subTitle
+        const defaultSub = subs.find(sub => !sub.subTitle);
+        if (defaultSub) {
+          if (subs.length==1) {
+            defaultSub.subTitle = this.getMajorCategory(majorType).title;
+          } else {
+            defaultSub.subTitle = 'etc';
+          }
+        }
+        
+        return subs;
+      },
+      onMajorCliick(majorType) {
         // when major-category is selected.
+        // Open subBar if closed?
+        this.majorVS.activeMajorType = majorType;
       }
     }
   }
